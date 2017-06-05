b430ccea52d7936f7eea854da71d50ee94ad06a8
stla <laurent_step@yahoo.fr>
2017-06-05 19:09:37 +0200

    cleaner code

--- src/BinarySplitting.hs
+++ src/BinarySplitting.hs
@@ -1,36 +1,25 @@
 module BinarySplitting
   (bsplitting)
   where
--- import Data.Ratio ((%))
 
+split0 :: ([Rational], [Rational]) -> [Rational]
+split0 u_v = map (\i -> (u !! (2*i)) * (v !! (2*i+1))) [0 .. m]
+  where (u, v) = u_v
+        m = div (length u) 2 - 1
 
-split1 :: [Rational] -> [Rational]
-split1 beta = map (\i -> (beta !! (2*i)) * (beta !! (2*i+1))) [0 .. m]
-  where n = length beta
-        m = div n 2 - 1
-
-split2 :: ([Rational], [Rational], [Rational]) -> [Rational]
-split2 adb = map
-              (\i -> (alpha !! (2*i)) * (beta !! (2*i+1)) +
-                     (delta !! (2*i)) * (alpha !! (2*i+1)))
-              [0 .. m]
-  where (alpha, delta, beta) = adb
-        n = length alpha
-        m = div n 2 - 1
-
-split3 :: ([Rational], [Rational], [Rational]) -> ([Rational], [Rational], [Rational])
-split3 adb = split adb (length alpha)
+split1 :: ([Rational], [Rational], [Rational]) ->
+               ([Rational], [Rational], [Rational])
+split1 adb = split adb (length alpha)
   where (alpha, _, _) = adb
-        split :: ([Rational], [Rational], [Rational]) -> Int -> ([Rational], [Rational], [Rational])
-        split uvw n =
+        split :: ([Rational], [Rational], [Rational]) -> Int ->
+                             ([Rational], [Rational], [Rational])
+        split u_v_w n =
           if n == 1
-            then uvw
-            else split (split2 uvw, split1 v, split1 w) (div n 2)
-          where (u, v, w) = uvw
+            then u_v_w
+            else split (x, split0 (v,v), split0 (w,w)) (div n 2)
+          where (u, v, w) = u_v_w
+                x  = zipWith (+) (split0 (u, w)) (split0 (v, u))
 
 bsplitting :: [Rational] -> [Rational] -> Rational
 bsplitting u v = num / den + 1
-  where ([num], _, [den]) = split3 (alpha, delta, beta)
-        alpha = u
-        delta = u
-        beta = v
+  where ([num], _, [den]) = split1 (u, u, v)
